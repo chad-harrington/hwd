3e33a15ac3601dcd77abd189b134674e3fe6c31a
Chad Harrington <chad@chadharrington.net>
2014-04-03 23:21:09 -0700

    Adds angular ui bootstrap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('hwdApp', [
+  'ui.bootstrap',
   'ngResource',
   'ngRoute'
 ])
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,7 @@
 
 angular.module('hwdApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+
+
   });
