d22e81388a2c9efd9bac56b80e4ce3ba443c088a
Chad Harrington <chad@chadharrington.net>
2014-05-24 10:12:12 -0700

    update carousel interval to 10s

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -4,7 +4,7 @@ angular.module('hwdApp')
 .controller('HomeCtrl', function ($scope, Skills, Services) {
 	
 	// Slider
-	$scope.slideInterval = -5000;
+	$scope.slideInterval = 10000;
 	$scope.slides = [
 		{ img: 'images/portfolio/mitsu_folio_slide_1.jpg', imgAlt: 'image alt', caption: 'caption 1'},
 		{ img: 'images/portfolio/mitsu_folio_slide_3.jpg', imgAlt: 'image alt', caption: 'caption 2'},
