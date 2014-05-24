3191bfcf8d44812073fe14a700253cc347be40be
Chad Harrington <chad@chadharrington.net>
2014-05-24 09:12:56 -0700

    add skills and services data to home controller

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,12 +1,20 @@
 'use strict';
 
 angular.module('hwdApp')
-.controller('HomeCtrl', function ($scope) {
-	$scope.slideInterval = 5000;
+.controller('HomeCtrl', function ($scope, Skills, Services) {
+	
+	// Slider
+	$scope.slideInterval = -5000;
 	$scope.slides = [
 		{ img: 'images/portfolio/mitsu_folio_slide_1.jpg', imgAlt: 'image alt', caption: 'caption 1'},
 		{ img: 'images/portfolio/mitsu_folio_slide_3.jpg', imgAlt: 'image alt', caption: 'caption 2'},
 		{ img: 'images/portfolio/vic_folio_slide_1.jpg', imgAlt: 'image alt', caption: 'caption 3'},
 		{ img: 'images/portfolio/vic_folio_slide_3.jpg', imgAlt: 'image alt', caption: 'caption 4'}
 	];
+
+	// Services
+	$scope.services = Services.query();
+	$scope.skills = Skills.query();
+
+
 });
