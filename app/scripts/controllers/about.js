6e4a86d1bb6ceb085dbcf638dd751496dce48e99
Chad Harrington <chad@chadharrington.net>
2014-06-02 22:26:07 -0700

    add skills rating

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,5 +7,23 @@ angular.module('hwdApp')
 		skills: Skills.query(),
 		education: Education.query()
 	};
+
+	$scope.rate = 3;
+	$scope.max = 5;
+	$scope.isReadonly = true;
+
+	$scope.hoveringOver = function(value) {
+		$scope.overStar = value;
+		$scope.percent = 100 * (value / $scope.max);
+	};
+
+	
+	$scope.ratingStates = [
+		{stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+		{stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+		{stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+		{stateOn: 'glyphicon-heart'},
+		{stateOff: 'glyphicon-off'}
+	];
 	
 });
